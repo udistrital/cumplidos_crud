b5447a34ddc28a6cd2d58ec344a351d017d332f3
juan camilo martinez <juacmartinezl@correo.udistrital.edu.co>
2022-02-14 08:12:42 -0500

    feature: se actualiza el commentRouter y swagger

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -412,6 +412,51 @@ func init() {
 			Filters:          nil,
 			Params:           nil})
 
+	beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           "/",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           "/",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           "/:id",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           "/:id",
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:FechasCargaCumplidosController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           "/:id",
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
 	beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:TrAprobacionMasivaDocumentosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_crud/controllers:TrAprobacionMasivaDocumentosController"],
 		beego.ControllerComments{
 			Method:           "Post",
